f3b85f481920349b47766771a243e7f0e6f893d3
Mark <gudimirallalokesh1@gmail.com>
2025-07-14 06:54:31 +0530

    Update attendanceData.js

--- attendanceData.js
+++ attendanceData.js
@@ -1,4 +1,7 @@
 const attendanceData = {
   "101": { attended: 36, total: 48 },
   "102": { attended: 40, total: 50 }
+  "12303023": { attended: 40, total: 50 }
+  "102": { attended: 40, total: 50 }
+  "102": { attended: 40, total: 50 }
 };
